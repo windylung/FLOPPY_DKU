b2247c7fdeead5bb39c3292605b803f31d77789d
windylung <jisoolee031@gmail.com>
2022-07-29 21:25:11 +0900

    get date and month

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -2,7 +2,9 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { COLOR_ORANGE } from "../colors";
-import Main from "../screens/main";
+// import Main from "../screens/main";
+import { format, formatDistance, formatRelative, subDays } from 'date-fns'
+
 
 
 const stamp = StyleSheet.create({
@@ -29,54 +31,73 @@ const stamp = StyleSheet.create({
 }
 
 )
-const ScreenMain = ({ navigation : {navigate} }) => (
-  <View>
-    <View style={{height: '10%', backgroundColor: COLOR_ORANGE}}>
-      <Image source={require("../screens/logo.png")} style={{width:'40%', height:'100%', marginTop: 10,overflow: 'hidden'}}></Image>
-    </View>
-    <View style={{ height: '100%', margin: 10}}>
-            <View>
-                <Text style={{fontSize : 20, fontWeight: "600"}}>MAY</Text>
-                <Text style={{fontSize : 20, textAlign: 'center'}}>4   5   6   7   8   9   10</Text>
-            </View>
+const ScreenMain = ({ navigation : {navigate} }) => {
+  let now = new Date();
+  let month = (now.getMonth() + 1).toLocaleString;
+  let monthString = now.toLocaleString("en-US", { month: "long" });
+  let date = now.getDate();
+  
 
-            <View>
-                <Text style = {{fontSize : 20}}>제작 현황</Text>
-                <View style={{width:'auto' ,height: 150, backgroundColor : COLOR_ORANGE, borderRadius: 10, opacity: 0.15}}>
-                </View>
-            </View>
-
-            <View style={{flexDirection: "row", height: 56, marginVertical: 10, justifyContent: 'space-around'}}>
-              <TouchableOpacity onPress={() => navigate("stamp")} style={{width:158 ,height: 56, backgroundColor : COLOR_ORANGE, borderRadius: 10, alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={{textAlign: 'center'}}>꽃다발 스탬프</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => navigate("planManagement")} style={{width:158 ,height: 56, backgroundColor : COLOR_ORANGE, borderRadius: 10, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>플랜 구독/관리</Text>
-              </TouchableOpacity>
-            </View>
-            <View>
-              <Text>주문한 꽃다발</Text>
-              <View style={{width:'auto', height: 150, backgroundColor : COLOR_ORANGE, borderRadius: 10, opacity: 0.15}}>
-                <Text>5월 12일(2회차)튤립</Text>
+  return (
+    <View>
+      <View style={{height: '10%', backgroundColor: COLOR_ORANGE}}>
+        <Image source={require("../screens/logo.png")} style={{width:'40%', height:'100%', marginTop: 10,overflow: 'hidden'}}></Image>
+      </View>
+      <View style={{ height: '100%', margin: 10}}>
+              <View>
+                  <Text style={{fontSize : 20, fontWeight: "600"}}>{monthString}</Text>
+                  <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+                    <Text>{date}</Text>
+                    <Text>{date + 1}</Text>
+                    <Text>{date + 2}</Text>
+                    <Text>{date + 3}</Text>
+                    <Text>{date + 4}</Text>
+                    <Text>{date + 5}</Text>
+                    <Text>{date + 6}</Text>
+                  </View>
               </View>
-            </View>
-            
-        </View>
-    {/* <TouchableOpacity onPress={() => navigate("stamp")}>
-      <Text>꽃다발 스탬프</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate("planManagement")}>
-      <Text>플랜구독</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate("order")}>
-      <Text>주문한 꽃다발</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate("news")}>
-      <Text>FLOPPY 소식</Text>
-    </TouchableOpacity>
-   */}
-  </View>
-);
+  
+              <View>
+                  <Text style = {{fontSize : 20}}>제작 현황</Text>
+                  <View style={{width:'auto' ,height: 150, backgroundColor : COLOR_ORANGE, borderRadius: 10, opacity: 0.15}}>
+                  </View>
+              </View>
+  
+              <View style={{flexDirection: "row", height: 56, marginVertical: 10, justifyContent: 'space-around'}}>
+                <TouchableOpacity onPress={() => navigate("stamp")} style={{width:158 ,height: 56, backgroundColor : COLOR_ORANGE, borderRadius: 10, alignItems: 'center', justifyContent: 'center'}}>
+                  <Text style={{textAlign: 'center'}}>꽃다발 스탬프</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigate("planManagement")} style={{width:158 ,height: 56, backgroundColor : COLOR_ORANGE, borderRadius: 10, alignItems: 'center', justifyContent: 'center'}}>
+                  <Text>플랜 구독/관리</Text>
+                </TouchableOpacity>
+              </View>
+              <View>
+                <Text>주문한 꽃다발</Text>
+                <View style={{width:'auto', height: 150, backgroundColor : COLOR_ORANGE, borderRadius: 10, opacity: 0.15}}>
+                  <Text>5월 12일(2회차)튤립</Text>
+                </View>
+              </View>
+              
+          </View>
+      {/* <TouchableOpacity onPress={() => navigate("stamp")}>
+        <Text>꽃다발 스탬프</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate("planManagement")}>
+        <Text>플랜구독</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate("order")}>
+        <Text>주문한 꽃다발</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate("news")}>
+        <Text>FLOPPY 소식</Text>
+      </TouchableOpacity>
+     */}
+    </View>
+  );
+}
+
+
+
 
   const ScreenStamp = () => 
   (
