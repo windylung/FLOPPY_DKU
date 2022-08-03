79dea480f316e3da420390592f2cca5805affde1
windylung <jisoolee031@gmail.com>
2022-08-03 23:56:09 +0900

    main screen

--- colors.js
+++ colors.js
@@ -1,1 +1,3 @@
 export const COLOR_ORANGE = "#ff9156";
+export const COLOR_GREY = "#00000066";
+export const COLOR_BG = "#f5f5f5";
--- navigation/Stack.js
+++ navigation/Stack.js
@@ -5,58 +5,73 @@ import {
   TouchableOpacity,
   StyleSheet,
   Image,
-  Dimensions,
-  useWindowDimensions,
+  PixelRatio
 } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { COLOR_ORANGE } from "../colors";
+import { COLOR_BG, COLOR_GREY, COLOR_ORANGE } from "../colors";
 import { ScreenWidth, ScreenHEIGHT } from "../resposiveScreen";
 import { format, formatDistance, formatRelative, subDays } from "date-fns";
 
 // const WINDOW_WIDTH = Dimensions.get("window").width;
 // const WINDOW_HEIGHT = Dimensions.get("window").height;
 
+const styles = StyleSheet.create({
+  title: {
+    // fontFamily: 
+    fontSize: 18,
+    fontWeight: "500",
+    color: 'black',
+  },
+  subTitle: {
+    
+    fontSize: 14,
+    fontWeight: "500",
+    color: COLOR_GREY,
+  },
+});
+
 const ScreenMain = ({ navigation: { navigate } }) => {
   // let now = new Date();
   // let month = (now.getMonth() + 1).toLocaleString;
   // let monthString = now.toLocaleString("en-US", { month: "long" });
   // let date = now.getDate();
-  console.log(ScreenHEIGHT(844));
+  console.log(PixelRatio.get())
   return (
     <View>
-      {/* top bar */}
-      <View style={{ height: ScreenHEIGHT(101), backgroundColor: COLOR_ORANGE }}>
-        <Image
-          source={require("../logo_orange.png")}
-          style={{ height: ScreenHEIGHT(36) }}
-        />
+    
+      <View style={{backgroundColor: COLOR_ORANGE, height: 101 ,justifyContent: 'flex-end', alignItems: 'flex-start'}}>
+        <Image source={require("../logo_orange.png")} style= {{height: ScreenHEIGHT(36), width:ScreenWidth(137)}}></Image>
       </View>
 
-      <View>{/* contents */}</View>
-      {/* Btn contents */}
+      <View style={{backgroundColor: COLOR_BG}}>
+      <View style={{backgroundColor: 'white', height: ScreenHEIGHT(162), width: ScreenWidth(350), marginHorizontal: ScreenWidth(20), marginVertical: ScreenHEIGHT(29)}}/>
       <View>
         <TouchableOpacity>
-          <Text>꽃다발 주문하기</Text>
-          <Text>꽃다발 예약</Text>
+          <Text style={styles.title}>꽃다발 주문하기</Text>
+          <Text style={styles.subTitle}>꽃다발 예약</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{width: ScreenWidth(165),height: ScreenHEIGHT(165), backgroundColor: COLOR_ORANGE}}>
+          <Text style={styles.title}>꽃다발 스탬프</Text>
+          <Text style={styles.subTitle}>주문 내역 확인</Text>
         </TouchableOpacity>
 
         <TouchableOpacity>
-          <Text>꽃다발 스탬프</Text>
-          <Text>주문 내역 확인</Text>
+          <Text style={styles.title}>플랜 구독/관리</Text>
+          <Text style={styles.subTitle}>꽃다발 주문 플랜 구독</Text>
         </TouchableOpacity>
 
         <TouchableOpacity>
-          <Text>플랜 구독/관리</Text>
-          <Text>꽃다발 주문 플랜 구독</Text>
+          <Text style={styles.title}>FLOPPY 이야기</Text>
+          <Text style={styles.subTitle}>FLOPPY의 소식</Text>
         </TouchableOpacity>
+      </View>        
+    </View>
+      
+      
 
-        <TouchableOpacity>
-          <Text>FLOPPY 이야기</Text>
-          <Text>FLOPPY의 소식</Text>
-        </TouchableOpacity>
-      </View>
 
-      <View></View>
+
     </View>
   );
 };
